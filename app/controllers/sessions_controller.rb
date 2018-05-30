14c83b01a485552ad6729d69a95c9da996a7fb45
Masa08 <ushimasarug08@gmail.com>
2018-05-30 01:09:09 +0000

    Implemented login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      redirect_to user_path(user.id)
+      log_in user
+      redirect_to user
     else
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @users = User.all
   end
 
   def show
@@ -13,15 +15,25 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to user_path(@user.id)
+      redirect_to @user
     else
       render 'new'
     end
-
   end
 
   def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update_attributes(user_params)
+      redirect_to current_user
+    else
+      render 'edit'
+    end
   end
 
   private
