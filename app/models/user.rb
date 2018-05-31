092675fec411c71ea13bda77b08d6009ae8a5145
Masataka Ushijima <ushimasarug08@gmail.com>
2018-05-31 13:10:55 +0800

    update user (#7)
    * update user
    
    * update only user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,11 @@
 class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   before_save { self.email = email.downcase }
-
   validates :name, presence: true,
                    length: { maximum: 50 }
-
   validates :email, presence: true,
                     length: { maximum: 255 },
                     uniqueness: { case_sensitive: false }
-
-  validates :password, presence: true, length: {minimum: 6}
-
+  validates :password, presence: true, length: {minimum: 6}, on: :create
   has_secure_password
 end
