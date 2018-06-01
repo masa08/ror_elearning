614c012109550671d240dac66d779e7c0934ab8a
Masa08 <ushimasarug08@gmail.com>
2018-06-01 07:18:56 +0000

    Implemented categories pages

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,9 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 9)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+end
