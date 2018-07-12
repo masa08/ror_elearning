4b1e284f29576bf6e1f03fa9603812251d763ca6
Masa08 <ushimasarug08@gmail.com>
2018-07-12 08:30:56 +0000

    Display activities in DashBoard

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,8 @@
 class TopController < ApplicationController
   def index
+      if logged_in?
+          @user = User.find(current_user.id)
+          @feeds = current_user.feed.paginate(page: params[:page], per_page: 10)
+      end
+    end
   end
-end
