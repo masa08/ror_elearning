4a11a4757b9f3f144b9d5e65f3c6e6dce027705d
Masa08 <ushimasarug08@gmail.com>
2018-08-04 07:01:00 +0000

    advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   mount_uploader :image, ImageUploader
   before_save { self.email = email.downcase }
   has_many :lesson, dependent: :destroy
@@ -24,6 +25,15 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum: 6}, on: :create
   has_secure_password
 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
   def feed
     following_ids  = "SELECT followed_id FROM relationships
                       WHERE follower_id = :user_id"
@@ -40,5 +50,19 @@ class User < ApplicationRecord
 
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute( :remember_digest, nil)
   end
 end
