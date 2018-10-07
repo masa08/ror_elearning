6a9df28e2bd8d8d39cab0ed3b70722ce3c6bde4c
Masa08 <ushimasarug08@gmail.com>
2018-10-07 06:47:14 +0000

    Update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
-100.times do
-  User.create(email: Forgery('email').address, password: Forgery(:basic).password, name: Forgery('internet').name ,image: Faker::Avatar.image)
+5.times do |n|
+  Category.create(
+    title: Faker::Commerce.unique.department,
+    description: Faker::Lorem.sentence(3)
+  )
+end
+
+User.create!(name: 'Masa', email: 'masa@gmail.com', password: 'password', admin: true)
+
+5.times do |n|
+  User.create!(name: Faker::Name.unique.name,
+        email: "user#{n}@gmail.com",
+        password: "password")
+end
+
+categories = Category.order("id DESC").take(5)
+
+categories.each do |category|
+  5.times do
+    content = Faker::Lorem.unique.word
+    word = category.words.build(content: content)
+
+    nth = rand(0..2)
+
+    3.times do |i|
+      if i == nth
+        word.word_answers.build(content: content, correct: true)
+      else
+        word.word_answers.build(content: Faker::Lorem.word)
+      end
+    end
+
+    word.save!
+  end
 end
