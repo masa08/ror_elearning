544715cbe5d42c9ea56156e0436d809aec256fa9
Masa08 <ushimasarug08@gmail.com>
2018-10-20 11:08:21 +0000

    Finished activities

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -10,8 +10,16 @@
 #
 
 class Relationship < ApplicationRecord
+    after_create :store_activity
+
     belongs_to :follower, class_name: "User"
     belongs_to :followed, class_name: "User"
     validates :follower_id, presence: true
     validates :followed_id, presence: true
+    has_one :activity, as: :action, dependent: :destroy
+
+    private
+    def store_activity
+        create_activity(user_id: follower.id)
+    end
 end
